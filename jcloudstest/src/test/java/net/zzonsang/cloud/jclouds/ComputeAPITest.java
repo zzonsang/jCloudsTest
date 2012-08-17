31b2b575e68aff6aea70d219351d2ace14ecaf56
zzonsang <zzonsang2@gmail.com>
2012-08-17 17:44:47 +0900

    cloudstack 3.0 hardware id

--- jcloudstest/src/test/java/net/zzonsang/cloud/jclouds/ComputeAPITest.java
+++ jcloudstest/src/test/java/net/zzonsang/cloud/jclouds/ComputeAPITest.java
@@ -23,18 +23,18 @@ public class ComputeAPITest {
 	/**
 	 * CloudStack 2.x
 	 */
-	private static final String ENDPOINT = "http://192.168.70.100:8080/client/api";
-	private static final String API_KEY = "TwvwiLBv56t7cowtj4LP6X9EMsJHDjeX8Ps6rtroRH9nXCIa0T_His_kJKzfTHDRAsfuWDmZ9B6Hwu0nLNXIzg";
-	private static final String SECRET_KEY = "jthFiw-bottzvOJMamhSF-vWuc7WB3OzwOq3Ws4KAt4R2Qz1kc0u7WfJcbt6XIc9vPmS8r_rZ7ew_MYubsx2ew";
-	private static final String HARDWARE_ID = "2";
+//	private static final String ENDPOINT = "http://192.168.70.100:8080/client/api";
+//	private static final String API_KEY = "TwvwiLBv56t7cowtj4LP6X9EMsJHDjeX8Ps6rtroRH9nXCIa0T_His_kJKzfTHDRAsfuWDmZ9B6Hwu0nLNXIzg";
+//	private static final String SECRET_KEY = "jthFiw-bottzvOJMamhSF-vWuc7WB3OzwOq3Ws4KAt4R2Qz1kc0u7WfJcbt6XIc9vPmS8r_rZ7ew_MYubsx2ew";
+//	private static final String HARDWARE_ID = "2";
 	
 	/**
 	 * CloudStack 3.x
 	 */
-//	private static final String ENDPOINT = "http://192.168.70.101:8080/client/api";
-//	private static final String API_KEY = "0vMUvC391sLZ17ghESlHGMbVpHkTFDDWW3a6xJPRrUKizY7OUs4Tcg54c9rpcGYz28GdRvL5sAjouzU5W_mDPw";
-//	private static final String SECRET_KEY = "28IAegR65_rM4R7AqSEMfA71gAxqa9sE54R_IjmvQuh1KfTUIS0HEO5gpvXE6HsnNF4O-wLlnPKUqfwVUX55Xg";
-//	private static final String HARDWARE_ID = "";
+	private static final String ENDPOINT = "http://192.168.70.101:8080/client/api";
+	private static final String API_KEY = "0vMUvC391sLZ17ghESlHGMbVpHkTFDDWW3a6xJPRrUKizY7OUs4Tcg54c9rpcGYz28GdRvL5sAjouzU5W_mDPw";
+	private static final String SECRET_KEY = "28IAegR65_rM4R7AqSEMfA71gAxqa9sE54R_IjmvQuh1KfTUIS0HEO5gpvXE6HsnNF4O-wLlnPKUqfwVUX55Xg";
+	private static final String HARDWARE_ID = "253bcf0e-dfac-46a1-81c2-0308c3579253";
 	
 	/**
 	 * 
