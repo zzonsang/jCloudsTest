3146e46a46753e5e22d904994395cc75f41704b6
zzonsang <zzonsang2@gmail.com>
2012-08-17 17:16:46 +0900

    init

---
+++ jcloudstest/src/test/java/net/zzonsang/cloud/jclouds/ComputeAPITest.java
@@ -0,0 +1,21 @@
+package net.zzonsang.cloud.jclouds;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputeAPITest {
+
+	/**
+	 * CloudStack 2.x
+	 */
+	private static final String HOST = "";
+	private static final String API_KEY = "";
+	private static final String SECRET_KEY = "";
+	
+	/**
+	 * CloudStack 3.x
+	 */
+	
+
+}
